5ddf3eb1a3ca1a90ad08876d72d5914b1feb0a5c
Wehner <veit.wehner@siemens.com>
2020-07-03 18:35:51 +0200

    stripped demo

--- src/main/java/botsandbytes/dashboard/backend/builder/ResponseBuilder.java
+++ src/main/java/botsandbytes/dashboard/backend/builder/ResponseBuilder.java
@@ -32,23 +32,19 @@ public class ResponseBuilder {
 
     private static List<String> getSecondaryColumns(Map<String, List<String>> pivotValues, List<ColumnVO> valueColumns) {
 
-        // create pairs of pivot col and pivot value i.e. (DEALTYPE,Financial), (BIDTYPE,Sell)...
         List<Set<Pair<String, String>>> pivotPairs = pivotValues.entrySet().stream()
                 .map(e -> e.getValue().stream()
                         .map(pivotValue -> Pair.of(e.getKey(), pivotValue))
                         .collect(toCollection(LinkedHashSet::new)))
                 .collect(toList());
 
-        // create cartesian product of pivot and value columns i.e. Financial_Sell_CURRENTVALUE, Physical_Buy_CURRENTVALUE...
         return Sets.cartesianProduct(pivotPairs)
                 .stream()
                 .flatMap(pairs -> {
-                    // collect pivot cols, i.e. Financial_Sell
                     String pivotCol = pairs.stream()
                             .map(Pair::getRight)
                             .collect(joining("_"));
 
-                    // append value cols, i.e. Financial_Sell_CURRENTVALUE, Financial_Sell_PREVIOUSVALUE
                     return valueColumns.stream()
                             .map(valueCol -> pivotCol + "_" + valueCol.getField());
                 })
